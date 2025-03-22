1a69d76bc01746bc5e741001c24455fe81c3e558
Alfredo <alfredo2.villarroya@gmail.com>
2025-03-22 12:20:37 +0000

    tested database access

--- src/pages/firestore/firestore.component.ts
+++ src/pages/firestore/firestore.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Server} from './server';
 import {FirestoreService} from '../../services/firestore.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-firestore',
   templateUrl: './firestore.component.html',
+  imports: [
+    NgForOf
+  ],
   styleUrls: ['./firestore.component.css']
 })
 export class FirestoreComponent implements OnInit {
@@ -14,8 +18,9 @@ export class FirestoreComponent implements OnInit {
 
   ngOnInit() {
     // Obtener datos en tiempo real
-    this.firestoreService.getCollection<Server>('servers').subscribe((data: Server[]) => {
+    this.firestoreService.getCollection('/servers').subscribe((data) => {
       this.servers = data;
     });
   }
+
 }
