5ffc895edbc29419d1e19c380f7cea05b891b6ce
Augusto <augustopaolone3@hotmail.com>
2025-03-23 11:30:18 +0000

    Añadido el diseño de planes de hosting con características

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {PaymentComponent} from './payment/payment.component';
+import {PlansComponent} from './plans/plans.component';
 
 export const routes: Routes = [
-  { path: "payment", component: PaymentComponent }
+  { path: "payment", component: PaymentComponent },
+  { path: "plans", component: PlansComponent }
 ];
