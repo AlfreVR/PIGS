5ae7db72e1bfb2f8f98f3c8ee648e99eca1e541d
Augusto <augustopaolone3@hotmail.com>
2025-04-03 22:01:41 +0100

    Añadido base footer,header,landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {PaymentComponent} from './payment/payment.component';
 import {PlansComponent} from './plans/plans.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingPageComponent},
   { path: "payment", component: PaymentComponent },
-  { path: "plans", component: PlansComponent }
+  { path: "plans", component: PlansComponent },
+  { path: '**', redirectTo: '' } // Redirecciona rutas desconocidas a la landing
+
 ];
