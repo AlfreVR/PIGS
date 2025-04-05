262f51911c48817c1fd6c187711506501fb56fd3
Agus <aagiribar@yahoo.es>
2025-04-05 15:51:45 +0100

    Added HTML for filter component

--- src/pages/components/filter/filter.component.ts
+++ src/pages/components/filter/filter.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  minCPUS: number = 2;
+  maxCPUS: number = 4;
 
+  minStorage: number = 256;
+  maxStorage: number = 512;
+
+  minMemory: number = 8;
+  maxMemory: number = 32;
+
+  minPrice: number = 2;
+  maxPrice: number = 16;
+
+  name: string = '';
 }
