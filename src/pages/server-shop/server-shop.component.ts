708c6f24e6a605f990992bf8ddf9435f7cff9549
Agus <aagiribar@yahoo.es>
2025-04-10 14:29:18 +0100

    Implemented serve-shop component to render filter and server list

--- src/pages/server-shop/server-shop.component.ts
+++ src/pages/server-shop/server-shop.component.ts
@@ -4,13 +4,15 @@ import {FirestoreService} from '../../services/firestore.service';
 import {NgForOf} from '@angular/common';
 import {Server} from '../firestore/server';
 import {ServerItemComponent} from '../components/server-item/server-item.component';
+import {FilterComponent} from '../components/filter/filter.component';
 
 @Component({
   selector: 'app-server-shop',
   templateUrl: './server-shop.component.html',
   imports: [
     NgForOf,
-    ServerItemComponent
+    ServerItemComponent,
+    FilterComponent
   ],
   styleUrls: ['./server-shop.component.css']
 })
